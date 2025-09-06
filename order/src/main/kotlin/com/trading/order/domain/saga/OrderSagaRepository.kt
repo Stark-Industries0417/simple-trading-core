a0cb13227dfaf3f65b58bec90e179d71f55260fa
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2025-09-06 14:40:57 +0900

    🔥 refactor: Order 모듈의 불필요한 Saga 도메인 클래스 제거
    - OrderSagaRepository와 OrderSagaState 엔티티 삭제
    - Outbox 패턴으로 전환하면서 더 이상 필요하지 않음

--- order/src/main/kotlin/com/trading/order/domain/saga/OrderSagaRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.trading.order.domain.saga
-
-import com.trading.common.domain.saga.SagaStateRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface OrderSagaRepository : SagaStateRepository<OrderSagaState>
